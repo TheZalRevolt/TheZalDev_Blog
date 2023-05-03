bfc59a7724a43bdb72d06c07cb3f28c70ea79d31
LAPTOP-OPL7SJUB\Riccardo Solazzi <riccardo.solazzi@revoltsrl.it>
2023-05-03 19:39:32 +0200

    layout changes

--- src/config.mjs
+++ src/config.mjs
@@ -7,7 +7,7 @@ const CONFIG = {
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
+  title: 'TheZal Blog',
   description:
     '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
   defaultImage: defaultImage,
